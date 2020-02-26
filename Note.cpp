3d9c42178e6e0e11ff0537665cc9256242bf98cf
Adenson <15594522896@163.com>
2020-02-26 17:31:14 +0800

    review

--- Note.cpp
+++ Note.cpp
@@ -228,3 +228,75 @@ using namespace std;
 //	return 0;
 //}
 //
+
+//#include <time.h>
+//struct A
+//{
+//	int a[10000];
+//};
+//A a;
+//A TestFunc1()
+//{
+//	return a;
+//}
+//A& TestFunc2()
+//{
+//	return a;
+//}
+//void TestReturnByRefOrValue()
+//{
+//	// 以值作为函数的返回值类型
+//	size_t begin1 = clock();
+//	for (size_t i = 0; i < 100000; ++i)
+//	{
+//    	TestFunc1();
+//	}
+//	size_t end1 = clock();
+//	// 以引用作为函数的返回值类型
+//	size_t begin2 = clock();
+//	for (size_t i = 0; i < 100000; ++i)
+//	{
+//      TestFunc2();
+//	}
+//	size_t end2 = clock();
+//	// 计算两个函数运算完成之后的时间
+//	cout << "TestFunc1 time:" << end1 - begin1 << endl;
+//	cout << "TestFunc2 time:" << end2 - begin2 << endl;
+//}
+//// 测试运行10次，值和引用作为返回值效率方面的区别
+//int main()
+//{
+//	for (int i = 0; i < 10; ++i)
+//	TestReturnByRefOrValue();
+//	system("pause");
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int a = 10;
+//
+//	int& ra = a;
+//	ra = 20;
+//
+//	int* pa = &a;
+//	*pa = 20;
+//
+//	system("pause");
+//	return 0;
+//}
+//int Add(int a, int b)
+//{
+//	return a + b;
+//}
+//int main()
+//{
+//	int ret = 0;
+//	ret = Add(5, 8);
+//	system("pause");
+//	return 0;
+//}
+//int TestAuto()
+//{ 
+//	return 10; 
+//}
